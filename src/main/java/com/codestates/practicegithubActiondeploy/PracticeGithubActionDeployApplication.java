a99c3ce25e5111eac33ea3932de221c342751263
junohheo <bbq0400@naver.com>
2022-10-07 16:06:39 +0900

    fu

--- src/main/java/com/codestates/practicegithubActiondeploy/PracticeGithubActionDeployApplication.java
+++ src/main/java/com/codestates/practicegithubActiondeploy/PracticeGithubActionDeployApplication.java
@@ -9,5 +9,6 @@ public class PracticeGithubActionDeployApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeGithubActionDeployApplication.class, args);
 	}
+	//ㅇㄹㅇㄹㅇㄹㅇㄹ
 
 }
